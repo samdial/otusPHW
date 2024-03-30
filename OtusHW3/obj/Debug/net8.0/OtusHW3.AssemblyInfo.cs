9e802e86bbaa9a08a921ee48398a973c274bba13
Samdial <afligirr@gmail.com>
2024-03-30 15:39:25 +0300

    1

--- OtusHW3/obj/Debug/net8.0/OtusHW3.AssemblyInfo.cs
+++ OtusHW3/obj/Debug/net8.0/OtusHW3.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OtusHW3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8ea5efac1c649349b8e61839a80b67602bb74054")]
 [assembly: System.Reflection.AssemblyProductAttribute("OtusHW3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OtusHW3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- solidhw-master/SolidHW/IGame.cs
+++
@@ -1,4 +0,0 @@
-﻿internal interface IGame
-{
-    void StartGame();
-}
